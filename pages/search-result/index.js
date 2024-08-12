0524490efed7f2810a4ea9c4f4e86c044ab6370f
starbvuks <starbvuks@gmail.com>
2024-08-12 15:19:16 +0530

    search loadin spinner

--- pages/search-result/index.js
+++ pages/search-result/index.js
@@ -1,15 +1,16 @@
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import Link from "next/link";
+import { Spinner } from "@nextui-org/react";
 import Navbar from "../../components/Navbar";
 import axios from "axios";
 
 const SearchResultsPage = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
-  // Extract the search term from the URL query parameters
   useEffect(() => {
     setSearchTerm(router.query.term || "");
   }, [router.query.term]);
@@ -18,27 +19,27 @@ const SearchResultsPage = () => {
     const token = process.env.NEXT_PUBLIC_TOKEN;
 
     const fetchSearchResults = async () => {
-      const res = await axios
-        .get(
+      setIsLoading(true);
+      try {
+        const res = await axios.get(
           `https://soad.alephinnovation.live/api/projects?populate=*&filters[$or][0][studentNames][$contains]=${searchTerm}&filters[$or][1][faculty][$contains]=${searchTerm}&filters[$or][2][keywords][$contains]=${searchTerm}&filters[$or][3][projectName][$contains]=${searchTerm}`,
           {
             headers: {
               Authorization: `Bearer ${token}`,
             },
           }
-        )
-        .then((res) => {
-          setSearchResults(res.data.data);
-          console.log(res.data.data)
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+        );
+        setSearchResults(res.data.data);
+        console.log(res.data.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
     if (searchTerm) {
       fetchSearchResults();
-      console.log(searchResults)
     } else {
       setSearchResults([]);
     }
@@ -51,31 +52,38 @@ const SearchResultsPage = () => {
         <span className="text-lg lg:text-2xl text-black font-light mt-24 border-b-3 border-dashed pb-6">
           Search Results For <span className="font-normal">"{searchTerm}"</span>
         </span>
-            <div className="grid grid-cols-2 gap-3 lg:gap-8 font-Outfit mt-8 xl:text-2xl xl:mt-10">
-              {searchResults.map((spec, index) => (
-                <Link
-                  href={{
-                    pathname: `/projects`,
-                    query: {
-                      specId: spec.attributes.specialization.data.id,
-                      semId: spec.attributes.semester.data.id,
-                      ayId: spec.attributes.academic_year.data.id,
-                      courseId: spec.attributes.course.data.id,
-                      projId: spec.id
-                    },
-                  }}
-                >
-                  <div className="relative font-Outfit transition border-2 lg:border-4 border-slate-500 text-slate-500 bg-slate-100 hover:bg-slate-500 hover:text-white hover:scale-105 flex flex-col justify-center py-9 px-4 lg:px-20 lg:py-16 h-full rounded-xl lg:rounded-3xl">
-                    <span className=" font-light italic text-xs lg:text-base">
-                      {spec.attributes.studentNames}
-                    </span>
-                    <span className=" font-semibold">
-                      {spec.attributes.projectName}
-                    </span>
-                  </div>
-                </Link>
-              ))}
-            </div>
+        {isLoading ? (
+          <div className="flex justify-center items-center h-64">
+            <Spinner color="primary" size="lg" className="z-100 pt-6" />
+          </div>
+        ) : (
+          <div className="grid grid-cols-2 gap-3 lg:gap-8 font-Outfit mt-8 xl:text-2xl xl:mt-10">
+            {searchResults.map((spec, index) => (
+              <Link
+                key={spec.id}
+                href={{
+                  pathname: `/projects`,
+                  query: {
+                    specId: spec.attributes.specialization.data.id,
+                    semId: spec.attributes.semester.data.id,
+                    ayId: spec.attributes.academic_year.data.id,
+                    courseId: spec.attributes.course.data.id,
+                    projId: spec.id
+                  },
+                }}
+              >
+                <div className="relative font-Outfit transition border-2 lg:border-4 border-slate-500 text-slate-500 bg-slate-100 hover:bg-slate-500 hover:text-white hover:scale-105 flex flex-col justify-center py-9 px-4 lg:px-20 lg:py-16 h-full rounded-xl lg:rounded-3xl">
+                  <span className="font-light italic text-xs lg:text-base">
+                    {spec.attributes.studentNames}
+                  </span>
+                  <span className="font-semibold">
+                    {spec.attributes.projectName}
+                  </span>
+                </div>
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
