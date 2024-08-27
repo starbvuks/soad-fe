6af05fa0a37f6f4622514c1cf3df10f39c44bd26
starbvuks <starbvuks@gmail.com>
2024-08-27 22:02:07 +0530

    speed insights

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { NextUIProvider } from "@nextui-org/react";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Head from "next/head";
 
 export default function App({
@@ -14,6 +15,7 @@ export default function App({
         <title>SoAD Archival Platform</title>
       </Head>
       <Analytics />
+      <SpeedInsights />
       <NextUIProvider>
         <Component {...pageProps} className="bg-[#fafafa]" />
       </NextUIProvider>
